5d73308598c5fd3a46eb9e2ca561088e449aee9c
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-13 14:33:49 +0000

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parserOptions': {
+        'ecmaVersion': 13,
+        'parser': '@typescript-eslint/parser',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
