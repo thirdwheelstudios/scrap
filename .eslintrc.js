452401e485de748ae6b32d8190e74a4bd67ba68a
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-16 21:51:14 +0000

    Linter changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,28 @@
 module.exports = {
-    'parser': 'vue-eslint-parser',
-    'globals': {
-        'defineProps': 'readonly'
-    },
-    'env': {
-        'node': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:vue/essential',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'parserOptions': {
-        'ecmaVersion': 13,
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'vue',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'vue/no-multiple-template-root': 'off'
-    }
+  parser: 'vue-eslint-parser',
+  globals: {
+    defineProps: 'readonly',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'vue/no-multiple-template-root': 'off',
+  },
 }
