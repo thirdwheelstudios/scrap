51be018ac45101562a9524dc46c2135802086b0a
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-23 10:45:08 +0000

    Added calendar icon

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import Store from './store'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faDesktop, faStopwatch } from '@fortawesome/free-solid-svg-icons'
+import { faDesktop, faStopwatch, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faDesktop, faStopwatch)
+library.add(faDesktop, faStopwatch, faCalendar)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
