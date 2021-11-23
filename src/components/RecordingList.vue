e583be917757ba2365a66eabfdac46c456a06e45
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-23 14:28:27 +0000

    List all recordings

--- src/components/RecordingList.vue
+++ src/components/RecordingList.vue
@@ -10,9 +10,11 @@ export default {
     const isRecording = computed(() => store.getters['isRecording'])
     const mediaStream = computed(() => store.getters['mediaStream'])
     const recordings = computed(() => {
-      const recordings: Recording[] = store.getters['recordings'] ?? []
+      const recordings: Recording[] = store.getters['recordings']
 
-      return recordings.sort((a, b) => a.startDateTime > b.startDateTime ? -1 : 1)
+      return recordings.sort((a, b) =>
+        a.startDateTime > b.startDateTime ? -1 : 1
+      )
     })
 
     store.dispatch('getRecordings')
@@ -28,17 +30,35 @@ export default {
 </script>
 
 <template>
-  <h2>My Scrapbook</h2>
-  <ul>
-    <li v-for="recording of recordings" :key="recording.id">
-      <RecordingItem :recording="recording" />
-    </li>
-  </ul>
+  <div class="container">
+    <h2>My Scrapbook</h2>
+    <ul>
+      <li v-if="!recordings.length">
+        It's looking quiet here, make a recording to get started!
+      </li>
+      <li v-else v-for="recording of recordings" :key="recording.id">
+        <RecordingItem :recording="recording" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.container {
+  background-color: #fff;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin: 2rem 25%;
+  text-align: left;
+  box-shadow: 0.1rem 0.1rem 0.5rem #dbe2f0;
+}
+
 ul {
   list-style: none;
   padding-left: 0;
+
+  li {
+    padding-top: 0.5rem;
+  }
 }
 </style>
