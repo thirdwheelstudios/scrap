4524b4e6b681b63cc1dde5bcbc164a15654a46fe
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-24 21:25:54 +0000

    Added icons for editing

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,25 @@ import App from './App.vue'
 import Store from './store'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faDesktop, faStopwatch, faCalendar, faFileDownload, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+  faStopwatch,
+  faCalendar,
+  faFileDownload,
+  faTrashAlt,
+  faPencilAlt,
+  faTimes,
+  faCheck,
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faDesktop, faStopwatch, faCalendar, faFileDownload, faTrashAlt)
+library.add(
+  faStopwatch,
+  faCalendar,
+  faFileDownload,
+  faTrashAlt,
+  faPencilAlt,
+  faTimes,
+  faCheck
+)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
