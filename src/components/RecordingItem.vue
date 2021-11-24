76b9041ba0131ca5080336dac98e921243850d6d
Phil Crossland <phil@thirdwheelstudios.com>
2021-11-24 21:33:15 +0000

    Initial edit mode logic

--- src/components/RecordingItem.vue
+++ src/components/RecordingItem.vue
@@ -35,9 +35,26 @@ export default {
       return props.recording.description ?? `Scrap #${props.recording.id}`
     })
 
+    const isInEditMode = ref(false)
+    const descriptionEle = ref(null)
+
     return {
       onDownloadClick: () => {
         store.dispatch('downloadRecording', props.recording.id)
+      },
+      onEditClick: () => {
+        isInEditMode.value = true
+        if (descriptionEle.value) {
+          const element: HTMLInputElement = descriptionEle.value
+          element.select()
+          element.focus()
+        }
+      },
+      onEditSaveClick: () => {
+        isInEditMode.value = false
+      },
+      onEditCancelClick: () => {
+        isInEditMode.value = false
       },
       onDeleteClick: () => {
         store.dispatch('deleteRecording', props.recording.id)
@@ -46,6 +63,8 @@ export default {
       recordingDate,
       description,
       imageSrc,
+      isInEditMode,
+      descriptionEle,
     }
   },
   components: { RecordingItemActionButton },
@@ -59,10 +78,40 @@ export default {
       <small>{{ duration }}</small>
     </div>
     <div class="content">
-      <span><input type="text" v-model="description" /></span>
+      <span
+        ><input
+          ref="descriptionEle"
+          type="text"
+          v-model="description"
+          :readonly="!isInEditMode"
+      /></span>
       <span><font-awesome-icon icon="calendar" /> {{ recordingDate }}</span>
       <ul>
-        <li>
+        <li v-if="isInEditMode">
+          <RecordingItemActionButton
+            icon="check"
+            @click="onEditSaveClick"
+            title="Save changes"
+            >Save</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="isInEditMode">
+          <RecordingItemActionButton
+            icon="times"
+            @click="onEditCancelClick"
+            title="Cancel changes"
+            >Cancel</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="!isInEditMode">
+          <RecordingItemActionButton
+            icon="pencil-alt"
+            @click="onEditClick"
+            title="Edit this recording"
+            >Edit</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="!isInEditMode">
           <RecordingItemActionButton
             icon="file-download"
             @click="onDownloadClick"
@@ -70,7 +119,7 @@ export default {
             >Download</RecordingItemActionButton
           >
         </li>
-        <li>
+        <li v-if="!isInEditMode">
           <RecordingItemActionButton
             icon="trash-alt"
             @click="onDeleteClick"
@@ -130,6 +179,10 @@ export default {
     input {
       border: 0;
       font-size: 1.5em;
+    }
+
+    input[readonly]:focus {
+      outline: none;
     }
   }
 }
