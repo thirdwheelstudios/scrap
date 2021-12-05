7830153ce7f38ee78ea12a2fc3e86a053ddd6dff
Phil Crossland <phil@thirdwheelstudios.com>
2021-12-05 16:23:35 +0000

    Feature/rename videos (#14)
    * Added icons for editing
    
    * Initial edit mode logic
    
    * Added update description to store
    
    * Implemented edit button to rename videos

--- src/components/RecordingItem.vue
+++ src/components/RecordingItem.vue
@@ -31,13 +31,44 @@ export default {
       URL.createObjectURL(props.recording.thumbnailBlob)
     )
 
-    const description = computed(() => {
-      return props.recording.description ?? `Scrap #${props.recording.id}`
-    })
+    const originalDescription = computed(
+      () => props.recording.description ?? `Scrap #${props.recording.id}`
+    )
+
+    const description = ref(originalDescription.value)
+
+    const isInEditMode = ref(false)
+    const descriptionEle = ref(null)
 
     return {
       onDownloadClick: () => {
         store.dispatch('downloadRecording', props.recording.id)
+      },
+      onEditClick: () => {
+        isInEditMode.value = true
+        if (descriptionEle.value) {
+          const element: HTMLInputElement = descriptionEle.value
+          element.select()
+          element.focus()
+        }
+      },
+      onProcessEditClick: (save: boolean) => {
+        if (save) {
+          const payload = {
+            id: props.recording.id,
+            description: description.value,
+          } as Recording
+          store.dispatch('updateDescription', payload)
+        } else {
+          description.value = originalDescription.value
+        }
+
+        if (descriptionEle.value) {
+          const element: HTMLInputElement = descriptionEle.value
+          element.focus()
+          element.setSelectionRange(0, 0)
+        }
+        isInEditMode.value = false
       },
       onDeleteClick: () => {
         store.dispatch('deleteRecording', props.recording.id)
@@ -46,6 +77,8 @@ export default {
       recordingDate,
       description,
       imageSrc,
+      isInEditMode,
+      descriptionEle,
     }
   },
   components: { RecordingItemActionButton },
@@ -59,10 +92,40 @@ export default {
       <small>{{ duration }}</small>
     </div>
     <div class="content">
-      <span><input type="text" v-model="description" /></span>
+      <span
+        ><input
+          ref="descriptionEle"
+          type="text"
+          v-model="description"
+          :readonly="!isInEditMode"
+      /></span>
       <span><font-awesome-icon icon="calendar" /> {{ recordingDate }}</span>
       <ul>
-        <li>
+        <li v-if="isInEditMode">
+          <RecordingItemActionButton
+            icon="check"
+            @click="onProcessEditClick(true)"
+            title="Save changes"
+            >Save</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="isInEditMode">
+          <RecordingItemActionButton
+            icon="times"
+            @click="onProcessEditClick(false)"
+            title="Cancel changes"
+            >Cancel</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="!isInEditMode">
+          <RecordingItemActionButton
+            icon="pencil-alt"
+            @click="onEditClick"
+            title="Edit this recording"
+            >Edit</RecordingItemActionButton
+          >
+        </li>
+        <li v-if="!isInEditMode">
           <RecordingItemActionButton
             icon="file-download"
             @click="onDownloadClick"
@@ -70,7 +133,7 @@ export default {
             >Download</RecordingItemActionButton
           >
         </li>
-        <li>
+        <li v-if="!isInEditMode">
           <RecordingItemActionButton
             icon="trash-alt"
             @click="onDeleteClick"
@@ -114,13 +177,21 @@ export default {
 
     span {
       display: block;
+      margin-top: 0.5rem;
+      margin-left: 0.5rem;
+    }
+
+    span:first-of-type {
+      margin: 0;
+      height: 2.5rem;
     }
 
     ul {
       list-style: none;
       padding-left: 0;
-      margin-top: 1rem;
       display: inline-flex;
+      margin-top: 0.5rem;
+      margin-left: 0.5rem;
 
       li {
         margin-right: 0.5rem;
@@ -128,8 +199,18 @@ export default {
     }
 
     input {
-      border: 0;
+      border: 0.1rem solid #d1d1d1;
+      border-radius: 0.25rem;
       font-size: 1.5em;
+      padding: 0.25rem;
+    }
+
+    input[readonly] {
+      border: 0;
+    }
+
+    input[readonly]:focus {
+      outline: none;
     }
   }
 }
