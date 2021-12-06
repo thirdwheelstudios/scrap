67e9ea5db9ca90737298b44ec04812be60187345
Phil Crossland <phil@thirdwheelstudios.com>
2021-12-06 15:40:32 +0000

    Changed modal background colour to grey

--- src/components/ModalContainer.vue
+++ src/components/ModalContainer.vue
@@ -43,7 +43,7 @@ export default {
   top: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(255, 255, 255, 0.4);
+  background-color: rgba(50, 50, 50, 0.4);
 
   button {
     position: absolute;
