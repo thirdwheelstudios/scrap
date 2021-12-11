d48f417af802dd6f7964d96b2a0c8cc8b53296a3
Phil Crossland <phil@thirdwheelstudios.com>
2021-12-11 16:47:51 +0000

    Check for null

--- src/composables/screenRecording.ts
+++ src/composables/screenRecording.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 
 export function screenRecording() {
-  const isSupported = ref(navigator && navigator.mediaDevices.getDisplayMedia)
+  const isSupported = ref(navigator && navigator.mediaDevices.getDisplayMedia !== null)
 
   return {
     isSupported,
