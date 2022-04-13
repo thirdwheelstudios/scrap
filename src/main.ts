22b13acbad916b4d43132639fc97e244fc272f62
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-13 21:59:29 +0100

    Setup router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
