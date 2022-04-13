9ef28949a5d2811e4c580c72e8ee18248e041d28
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-13 22:51:55 +0100

    Started theming

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/settings">Settings</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/settings">Settings</router-link>
   </div>
   <router-view />
 </template>
 
-<style>
+<style type="scss">
+@import './assets/styles/_variables.scss';
+
+html body {
+  margin: 0;
+}
+
+html {
+  background-color: var(--bg-canvas);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
