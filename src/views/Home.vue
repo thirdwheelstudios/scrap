f812fb2c651f691b4146e29eaafb629a4ea06c29
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-20 14:40:35 +0100

    Added initial record button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,23 @@ const { isSupported } = screenRecording()
 </script>
 
 <template>
-  <MonitorContainer v-if="isSupported" :power-on="false"></MonitorContainer>
+  <MonitorContainer v-if="isSupported" :power-on="false">
+    <div class="monitor-screen"><button></button></div>
+  </MonitorContainer>
   <BrowserNotSupported v-else></BrowserNotSupported>
 </template>
+
+<style scoped lang="scss">
+.monitor-screen {
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  button {
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    margin: auto;
+  }
+}
+</style>
