75e9a5ffd4386ce7245614d43711ff957acf7178
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-20 08:36:11 +0100

    Added 'browser not supported' component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import MonitorContainer from '../components/MonitorContainer.vue'
+import { screenRecording } from '../composables/screenRecording'
+import BrowserNotSupported from '../components/BrowserNotSupported.vue'
+
+const { isSupported } = screenRecording()
 </script>
 
 <template>
-  <MonitorContainer :power-on="false"></MonitorContainer>
+  <MonitorContainer v-if="isSupported" :power-on="false"></MonitorContainer>
+  <BrowserNotSupported v-else></BrowserNotSupported>
 </template>
