423243ebcd77d464e7bdc4e3555a448d6c73beb8
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-29 22:55:02 +0100

    Created radial gradient container

---
+++ src/components/GradientContainer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  background-image: radial-gradient(
+    rgba(0, 0, 0, 0.1),
+    rgba(0, 0, 0, 0.2),
+    rgba(0, 0, 0, 0.3)
+  );
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-radius: 0.25rem;
+}
+</style>
