4dedadd610dc82c870f2f7a09f79e1e9aeacaaff
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-30 08:54:17 +0100

    Swapped out for linear gradient

--- src/components/GradientContainer.vue
+++ src/components/GradientContainer.vue
@@ -8,12 +8,12 @@
 
 <style scoped lang="scss">
 div {
-  background-image: radial-gradient(
-    rgba(0, 0, 0, 0.1),
+  background-image: linear-gradient(
+    rgba(0, 0, 0, 0.05) 50%,
     rgba(0, 0, 0, 0.2),
     rgba(0, 0, 0, 0.3)
   );
-  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
   border-radius: 0.25rem;
 }
 </style>
