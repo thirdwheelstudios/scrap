03e68991e5844353d3f49b4e2fead365a47753f9
Phil Crossland <phil@thirdwheelstudios.com>
2022-04-30 10:31:51 +0100

    Added font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).mount('#app')
+library.add(faFileDownload)
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(router)
+  .use(pinia)
+  .mount('#app')
