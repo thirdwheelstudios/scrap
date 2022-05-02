63a7efef8495e647b18ea313b985ed4c739642ea
Phil Crossland <phil@thirdwheelstudios.com>
2022-05-02 22:13:13 +0100

    Added delete option to video

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
+import { faFileDownload, faTrash } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
 const pinia = createPinia()
 
-library.add(faFileDownload)
+library.add(faFileDownload, faTrash)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
