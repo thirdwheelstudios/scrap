2ff42b679d92e1523ffa668ea9d4e15014fb1cc7
Phil Crossland <phil@thirdwheelstudios.com>
2022-05-02 21:58:16 +0100

    Detect mobile and show 'not supported' dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import BrowserNotSupported from '../components/BrowserNotSupported.vue'
 import MainWindow from '../components/MainWindow.vue'
+import { deviceDetection } from '../composables/deviceDetection'
 import { screenRecording } from '../composables/screenRecording'
 
 const { isSupported } = screenRecording()
+const { isMobile } = deviceDetection()
+
+const deviceIsSupported = computed(() => isSupported.value && !isMobile.value)
 </script>
 
 <template>
-  <MainWindow v-if="isSupported" />
+  <MainWindow v-if="deviceIsSupported" />
   <BrowserNotSupported v-else />
 </template>
 
