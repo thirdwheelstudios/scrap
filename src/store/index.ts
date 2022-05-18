e5750dcc377e1da2815130d8e11342730bd1e31f
Phil Crossland <phil@thirdwheelstudios.com>
2022-05-18 21:02:40 +0100

    Added modal store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+export * from './modal'
 export * from './settings'
 export * from './recording'
 export * from './recordingsList'
---
+++ src/store/modal/index.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+
+export const useModalStore = defineStore('modal', {
+  state: () => {
+    let currentModal: string | undefined
+    let currentProps
+
+    return { currentModal, currentProps }
+  },
+  getters: {
+    current(state) {
+      return state.currentModal
+    },
+    props(state) {
+      return state.currentProps
+    },
+  },
+  actions: {
+    open(modal: string, props: any) {
+      this.currentModal = modal
+      this.currentProps = props
+    },
+    close() {
+      this.currentModal = undefined
+      this.currentProps = undefined
+    },
+  },
+})
