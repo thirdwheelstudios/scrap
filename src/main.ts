d9f09dc913f747790cbc6b87694634248d6dce21
Phil Crossland <phil@thirdwheelstudios.com>
2022-05-26 20:59:33 +0100

    Added play icon

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFileDownload, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faFileDownload, faTrash, faPlay } from '@fortawesome/free-solid-svg-icons'
 import DeleteRecordingModal from './components/modals/DeleteRecordingModal.vue'
 import DeleteAllRecordingsModal from './components/modals/DeleteAllRecordingsModal.vue'
 import App from './App.vue'
@@ -10,7 +10,7 @@ import router from './router'
 
 const pinia = createPinia()
 
-library.add(faFileDownload, faTrash)
+library.add(faFileDownload, faTrash, faPlay)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
