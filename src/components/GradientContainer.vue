1eabce3a21d18eb488ffbe9c4618ca6a7495b991
Phil Crossland <phil@thirdwheelstudios.com>
2022-06-10 14:26:45 +0100

    Lightened background gradient (#41)

--- src/components/GradientContainer.vue
+++ src/components/GradientContainer.vue
@@ -9,9 +9,9 @@
 <style scoped lang="scss">
 div {
   background-image: linear-gradient(
-    rgba(0, 0, 0, 0.15) 50%,
-    rgba(0, 0, 0, 0.2),
-    rgba(0, 0, 0, 0.3)
+    rgba(0, 0, 0, 0.1) 50%,
+    rgba(0, 0, 0, 0.15),
+    rgba(0, 0, 0, 0.1)
   );
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
   border-radius: 0.25rem;
