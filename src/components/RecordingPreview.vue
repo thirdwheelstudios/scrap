47348c5ec6db10909612d1e17bc04d3dbf3d79af
Phil Crossland <phil@thirdwheelstudios.com>
2022-06-14 22:00:25 +0100

    Thumbnails that don't fit the aspect ratio for thumbnails are re-sized and centered

--- src/components/RecordingPreview.vue
+++ src/components/RecordingPreview.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { computed, ref, watchEffect } from 'vue'
+import { screenRecording } from '../composables/screenRecording'
 import { useRecordingStore } from '../store'
 
 const recording = useRecordingStore()
+const { 
+  resizeCoordsForThumbnail, 
+  thumbnailHeight, 
+  thumbnailWidth,
+} = screenRecording()
 
 const mediaStream = computed(() => recording.mediaStream)
 
@@ -16,12 +22,24 @@ watchEffect(() => {
   const video = videoEle.value
 
   setTimeout(() => {
-    canvas.width = video.clientWidth
-    canvas.height = video.clientHeight
+    canvas.width = thumbnailWidth.value
+    canvas.height = thumbnailHeight.value
     const context = canvas.getContext('2d')
     if (!context) return
 
+    context.globalAlpha = 0.1
+    context.fillStyle = '#FFFFFF'
+    context.fillRect(0, 0, canvas.width, canvas.height)
     context.drawImage(video, 0, 0, canvas.width, canvas.height)
+    
+    const { height, width } = resizeCoordsForThumbnail(video.videoHeight, video.videoWidth)
+
+    const xOffset = (thumbnailWidth.value - width) / 2
+    const yOffset = (thumbnailHeight.value - height) / 2
+
+    context.globalAlpha = 1
+    context.drawImage(video, xOffset, yOffset, canvas.width - (xOffset * 2), canvas.height - (yOffset * 2))
+
     canvas.toBlob((blob) => recording.setThumbnail(blob!))
   }, 500)
 })
