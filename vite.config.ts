ef4bc6cb4683d614b342662245d5a319b5fcb0a2
Phil Crossland <phil@thirdwheelstudios.com>
2022-06-26 22:18:01 +0100

    Added icons and PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Scrap Screen Recorder',
+        short_name: 'Scrap',
+        description: 'Use Scrap to record any screen or window directly from your browser',
+        theme_color: '#3f3959',
+        background_color: '#36314f',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
