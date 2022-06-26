d0b50681e72c1d1d64781be2637ab9e0de7bd481
Phil Crossland <phil@thirdwheelstudios.com>
2022-06-26 22:24:47 +0100

    Fixed icon names

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
         background_color: '#36314f',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
