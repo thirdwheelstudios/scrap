0c05abcc3638bb0be11703c159ef7a4982e43476
Phil Crossland <phil@thirdwheelstudios.com>
2022-06-26 18:35:32 +0100

    Feature/monitor component UI (#49)
    * Reduced width
    
    * Tweaked positioning
    
    * added screen glare as an overlay

--- src/components/RecordingPreview.vue
+++ src/components/RecordingPreview.vue
@@ -58,9 +58,11 @@ watchEffect(() => {
 
 <style scoped lang="scss">
 div {
-  width: calc(100% - 1.2rem);
-  height: calc(100% - 3rem);
   position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
 
   video {
     height: 100%;
